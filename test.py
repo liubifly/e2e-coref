76651594040e1ad620be7cf336f2e0d1f68d6857
Bifei <liubifly@hotmail.com>
2019-06-10 17:29:37 -0400

    fixed bugs

--- test.py
+++ test.py
@@ -19,17 +19,13 @@ if __name__ == "__main__":
   with tf.Session() as session:
     model.restore(session)
 
-  with open(input_filename) as input_file:
-    for example_num, line in enumerate(input_file.readlines()):
-      example = json.loads(line)
-      tensorized_example = model.tensorize_example(example, is_training=False)
-      feed_dict = {i:t for i,t in zip(model.input_tensors, tensorized_example)}
-      _, _, _, top_span_starts, top_span_ends, top_antecedents, top_antecedent_scores = session.run(model.predictions, feed_dict=feed_dict)
-      predicted_antecedents = model.get_predicted_antecedents(top_antecedents, top_antecedent_scores)
-      example["predicted_clusters"], _ = model.get_predicted_clusters(top_span_starts, top_span_ends, predicted_antecedents)
-      print(example['predicted_clusters'])
-      break
-
-      if example_num % 100 == 0:
-        print("Decoded {} examples.".format(example_num + 1))
+    with open(input_filename) as input_file:
+      for example_num, line in enumerate(input_file.readlines()):
+        example = json.loads(line)
+        tensorized_example = model.tensorize_example(example, is_training=False)
+        feed_dict = {i:t for i,t in zip(model.input_tensors, tensorized_example)}
+        _, _, _, top_span_starts, top_span_ends, top_antecedents, top_antecedent_scores = session.run(model.predictions, feed_dict=feed_dict)
+        predicted_antecedents = model.get_predicted_antecedents(top_antecedents, top_antecedent_scores)
+        example["predicted_clusters"], _ = model.get_predicted_clusters(top_span_starts, top_span_ends, predicted_antecedents)
+        print(example['predicted_clusters'])
 
