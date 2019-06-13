6bb70f5c0705f2e653e2a044cd5165e1efef0c2d
Bifei <liubifly@hotmail.com>
2019-06-12 23:21:13 -0400

    print

--- train.py
+++ train.py
@@ -40,8 +40,7 @@ if __name__ == "__main__":
                                                                    model.tmp2])
       accumulated_loss += tf_loss
 
-      print('tmp', tf_tmp.shape)
-      print('tmp2', tf_tmp2.shape)
+      print('tmp', tf_tmp)
 
       if tf_global_step % report_frequency == 0:
         total_time = time.time() - initial_time
